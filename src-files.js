2ed256844bae2b5a5d780cedee846a991a47c215
Simeon H.K. Fitch <fitch@astraea.io>
2023-12-12 16:04:49 -0500

    Documentation update Tue Dec 12 16:04:49 EST 2023

--- src-files.js
+++ src-files.js
@@ -1,14 +1,4 @@
 var srcIndex = JSON.parse('{\
-"erased_cells":["",[["masked",[],["mask.rs","masked_buffer.rs","mod.rs","nodata.rs"]]],["buffer.rs","ctype.rs","encoding.rs","error.rs","lib.rs","value.rs"]],\
-"num_traits":["",[["ops",[],["bytes.rs","checked.rs","euclid.rs","inv.rs","mod.rs","mul_add.rs","overflowing.rs","saturating.rs","wrapping.rs"]]],["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]],\
-"paste":["",[],["attr.rs","error.rs","lib.rs","segment.rs"]],\
-"proc_macro2":["",[],["detection.rs","extra.rs","fallback.rs","lib.rs","marker.rs","parse.rs","rcvec.rs","wrapper.rs"]],\
-"quote":["",[],["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]],\
-"serde":["",[["de",[],["format.rs","ignored_any.rs","impls.rs","mod.rs","seed.rs","size_hint.rs","value.rs"]],["private",[],["de.rs","doc.rs","mod.rs","ser.rs"]],["ser",[],["fmt.rs","impls.rs","impossible.rs","mod.rs"]]],["integer128.rs","lib.rs","macros.rs"]],\
-"serde_derive":["",[["internals",[],["ast.rs","attr.rs","case.rs","check.rs","ctxt.rs","mod.rs","receiver.rs","respan.rs","symbol.rs"]]],["bound.rs","de.rs","dummy.rs","fragment.rs","lib.rs","pretend.rs","ser.rs","this.rs"]],\
-"syn":["",[["gen",[],["clone.rs"]]],["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","drops.rs","error.rs","export.rs","expr.rs","ext.rs","gen_helper.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","meta.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","restriction.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]],\
-"thiserror":["",[],["aserror.rs","display.rs","lib.rs"]],\
-"thiserror_impl":["",[],["ast.rs","attr.rs","expand.rs","fmt.rs","generics.rs","lib.rs","prop.rs","span.rs","valid.rs"]],\
-"unicode_ident":["",[],["lib.rs","tables.rs"]]\
+"erased_cells":["",[["masked",[],["mask.rs","masked_buffer.rs","mod.rs","nodata.rs"]]],["buffer.rs","ctype.rs","encoding.rs","error.rs","lib.rs","value.rs"]]\
 }');
 createSrcSidebar();
