188a15a12c62529e4a38bc22014e08348e88a878
Simeon H.K. Fitch <fitch@astraea.io>
2024-01-01 15:37:18 -0500

    Optional dependency build fix.

--- src/lib.rs
+++ src/lib.rs
@@ -34,10 +34,8 @@
 //! |:--------:|------------------------------------------------|:-------:|
 //! | `masked` | Enable the `MaskedCellBuffer` API              | `true`  |
 //! | `serde`  | Derive `serde` traits for core types           | `true`  |
-//! | `gdal`   | Enable `CellBuffer`s in `georust/gdal` API[^1] | `false` |
+//! | `gdal`   | Enable `CellBuffer`s in `georust/gdal` API     | `false` |
 //!
-//! [^1]: Note: `gdal` disables cell-types `UInt64`, `Int8`, and `Int64`
-//! to be backwards compatible with older versions of GDAL.
 
 mod buffer;
 mod ctype;
@@ -59,7 +57,29 @@ pub use masked::*;
 use std::fmt::{Debug, Formatter};
 pub use value::*;
 
-#[doc=include_str!("with_ct.md")]
+/// `with_ct` is a [callback style](https://danielkeep.github.io/tlborm/book/pat-callbacks.html)
+/// macro used to construct various implementations covering all [`CellType`]s.
+///
+/// It calls the passed identifier as a macro with two parameters:
+/// * the cell type id (e.g. `UInt8`),
+/// * the cell type primitive (e.g. `u8`).
+///
+/// # Example
+/// ```rust
+/// use erased_cells::{with_ct, CellType};
+/// fn primitive_name(ct: CellType) -> &'static str {
+///     macro_rules! primitive_name {
+///        ($(($id:ident, $p:ident)),*) => {
+///             match ct {
+///                 $(CellType::$id => stringify!($p),)*
+///             }
+///        };
+///     }
+///     with_ct!(primitive_name)
+/// }
+///
+/// assert_eq!(primitive_name(CellType::Float32), "f32");
+/// ```
 #[macro_export]
 macro_rules! with_ct {
     ($callback:ident) => {
